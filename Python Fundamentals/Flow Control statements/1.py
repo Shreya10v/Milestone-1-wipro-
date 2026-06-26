24952c3f3be72a47654fb7adff4476ebdd7fd9ac
Shreya Vishwakarma <shreya102005@gmail.com>
2026-06-26 21:31:09 +0530

    Create 1.py

---
+++ Python Fundamentals/Flow Control statements/1.py
@@ -0,0 +1,8 @@
+#WAP to check if a no. is +ve, -ve or zero
+num=int(input("Enter a number: "))
+if num>0:
+    print("The number is positive")
+elif num<0:
+    print("The number is negative")
+else:
+    print("The number is zero")
